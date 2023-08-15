a9d67b2911253c51fd6d25e2b6ee6c60d1beea3f
Arun Kumar R <aruntutter19@gmail.com>
2023-08-15 22:19:00 +0530

    Designed Responsive About Section

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,16 @@ const About = () => {
       <div className="about-description">
         <h1>About</h1>
         <p>
-          "I'm driven by a passion for Front-End Development, creating immersive
-          UIs that seamlessly merge design and functionality. With a commitment
-          to continuous learning, I strive to deliver responsive, engaging web
-          experiences that delight users."
+          "I am deeply passionate about Front-End Development, with a focused
+          expertise in crafting immersive user interfaces. My proficiency lies
+          in creating responsive, aesthetically pleasing web experiences that
+          seamlessly merge design and functionality. My commitment to continuous
+          learning drives me to deliver meticulously designed front-end
+          solutions, enhancing user engagement and satisfaction."
         </p>
+        <div className="contact-btn ">
+          <a href="#about">Connect</a>
+        </div>
       </div>
       <div className="about-img">
         <img src={require("../assets/images/about-img.jpg")} alt="about-img" />
