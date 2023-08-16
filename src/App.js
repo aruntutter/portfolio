1478e6b3ccf9e876121d2447c4c6a462be85b0c5
Arun Kumar R <aruntutter19@gmail.com>
2023-08-16 15:41:10 +0530

    Footer component imported

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 };
