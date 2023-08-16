8091f3f29fe0f2d5a3b05b31a3fca69aa40d51d7
Arun Kumar R <aruntutter19@gmail.com>
2023-08-16 12:17:36 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </>
   );
 };
